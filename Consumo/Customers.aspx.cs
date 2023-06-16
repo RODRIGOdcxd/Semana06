4e5af8e179127bdb668ee18b910dfb1807a87143
RODRIGOdcxd <diazrodrigodc@gmail.com>
2023-06-15 20:31:02 -0500

    Commit 2

--- Consumo/Customers.aspx.cs
+++ Consumo/Customers.aspx.cs
@@ -4,12 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Consumo.ServiceReference1;
 namespace Consumo
 {
     public partial class Customers : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+
+        private void BindGrid()
+        {
+            Service1Client client = new Service1Client();
+            GridView1.DataSource = client.Get().CustomersTable;
+            GridView1.DataBind();
+        }
+
+        protected void OnRowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void Insert(object sender, EventArgs e)
         {
 
         }
